41ed64c4e373dc726ca0d5d488546fff405486a7
Nikolai <sealmove95@yandex.ru>
2023-01-12 01:46:55 +0300

    Task 10

---
+++ Program10.cs
@@ -0,0 +1,8 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+int num2 = 0;
+if (num < 100)
+  {
+    Console.WriteLine("ERROR! Please enter num > 99!");
+  }
+num2 = num / 10 % 10;
+Console.WriteLine(num2);

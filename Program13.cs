1ad22f23a1e71cc273e84ea980121ec48481ea44
Nikolai Kuznetsov <110773920+Sealmove95@users.noreply.github.com>
2023-01-12 01:50:38 +0300

    Task 13

---
+++ Program13.cs
@@ -0,0 +1,8 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+int num2 = 0;
+if (num < 100)
+  {
+    Console.WriteLine("We don't have number 3!");
+  }
+num2 = num % 10;
+Console.WriteLine(num2);
